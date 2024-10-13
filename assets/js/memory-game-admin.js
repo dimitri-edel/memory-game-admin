427228393307cf9666217d708258c8c4b89659cb
dimitri-edel <dimitri.edel@gmx.net>
2024-10-14 00:08:48 +0200

    Make items in the table editable and deleteable
    I have also added some styles

--- assets/js/memory-game-admin.js
+++ assets/js/memory-game-admin.js
@@ -1,21 +1,108 @@
 // The resultsJSON variable is used in pagination.js
 var resultsJSON = [];
+var selected_item = null;
+
+function editItem(item) {
+    let row = document.getElementById("row-" + item.id);
+    // Parse the item if it is a JSON string
+    if (typeof item === 'string') {
+        item = JSON.parse(item);    
+    }
+    // If there is a selected item, unselect it
+    if (selected_item !== null) {
+        unselectItem(selected_item);
+    }
+    // Select the current item
+    selected_item = item;
 
 
-async function updateItem() {
-    const id = document.getElementById("update_id").value;
-    const category = document.getElementById("update_category").value;
-    const audio = document.getElementById("update_audio").files[0];
-    const title = document.getElementById("update_title").value;
-    const description = document.getElementById("update_description").value;
-    const image = document.getElementById("update_image").files[0];
+    row.innerHTML = `
+                <td id="update_id" class="selected-row">${item.id}</td>
+                <td class="selected-row"><input  id="update_category" type="text" value="${item.category}" required></td>
+                <td class="selected-row">
+                    <span id="audio-select" style="display:none" class="audio-update">
+                        <label for="audio">Audio:</label>
+                        <input type="file" id="update_audio" accept="audio/*" onchange="hideInitialFilename('initial_audio_filename')">
+                        <button onclick="cancelSelection('audio-select', 'intial_audio_filename')"><i class="fa-regular fa-rectangle-xmark"></i></button>                        
+                    </span>
+                    <!-- Element to display the initial file name -->
+                    <span id="intial_audio_filename">                        
+                        <button value="Play" onclick="play_audio('${base_url}${item.audio}')">Play</button>
+                        <button value="Change" onclick="hideInitialFilename('intial_audio_filename', 'audio-select')">Change</button>
+                    </span>
+                </td>                
+                <td class="selected-row"><input id="update_title" type="text" value="${item.title}" required></td>
+                <td class="selected-row"><input  id="update_description" type="text" value="${item.description}" required></td>
+                <td class="selected-row">
+                    <span id="image-select"  style="display:none" class="image-update">
+                        <label for="image">Image:</label>
+                        <input type="file" id="update_image" accept="image/*">
+                        <button onclick="cancelSelection('image-select', 'intial_image_section')"><i class="fa-regular fa-rectangle-xmark"></i></button>
+                    </span>
+                    <!-- Element to display the initial file name -->
+                    <span id="intial_image_section" class="image-update">                        
+                        <img src="${base_url}${item.image}" alt="${item.title}" width="100">
+                        <button value="Change" onclick="hideInitialFilename('intial_image_section', 'image-select')">Change</button>
+                    </span>
+                </td>                
+                <td class="selected-row"><span class="update-button" onclick="updateItem(${item.id})"><i class="fa-solid fa-cloud-arrow-up"></i></span></td>
+                <td class="selected-row"><span class="delete-button" onclick="deleteItem(${item.id})"><i class="fa-solid fa-trash-can"></i></span></td>
+            `;
+}
+
+function unselectItem(item) {
+    let row = document.getElementById("row-" + item.id);
+    row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.category}</td>
+                <td><button onclick="play_audio('${base_url}${item.audio}')"> Play</button></td>
+                <td>${item.title}</td>
+                <td>${item.description}</td>
+                <td><img src="${base_url}${item.image}" alt="${item.title}" width="100"></td>
+                <td><span class="edit-button" onclick='editItem(${JSON.stringify(item)})'><i class="fa-solid fa-pen-to-square"></i></span></td>
+                <td><span class="delete-button" onclick="deleteItem(${item.id})"><i class="fa-solid fa-trash-can"></i></span></td>
+            `;
+}
+
+function cancelSelection(select_section_id, intial_section_id) {
+    document.getElementById(select_section_id).style.display = "none";
+    document.getElementById(intial_section_id).style.display = "flex";
+}
+
+function hideInitialFilename(intial_section_id, select_section_id) {
+    document.getElementById(intial_section_id).style.display = "none";
+    document.getElementById(select_section_id).style.display = "flex";
+}
+
+async function updateItem(id) {
+    // const id = document.getElementById("update_id").value;
+    // const category = document.getElementById("update_category").value;
+    // const audio = document.getElementById("update_audio").files[0];
+    // const title = document.getElementById("update_title").value;
+    // const description = document.getElementById("update_description").value;
+    // const image = document.getElementById("update_image").files[0];
+    // console.log(`updating category: ${category}`);
+
+    const row = document.getElementById("row-" + id);
+    const category = row.querySelector("#update_category").value;
+    const audio = row.querySelector("#update_audio");
+    const title = row.querySelector("#update_title").value;
+    const description = row.querySelector("#update_description").value;
+    const image = row.querySelector("#update_image");
+    console.log(`updating category: ${category}`);
 
     const formData = new FormData();
     formData.append("category", category);
-    formData.append("audio", audio);
+    // Check if a new audio file has been selected
+    if (audio.files.length > 0) {
+        formData.append("audio", audio.files[0]);
+    }
     formData.append("title", title);
     formData.append("description", description);
-    formData.append("image", image);
+    // Check if a new image file has been selected
+    if (image.files.length > 0) {
+        formData.append("image", image.files[0]);
+    }
 
     const token1 = getCookie("token1");
     const token2 = getCookie("token2");
@@ -91,7 +178,7 @@ async function deleteItem() {
     const id = document.getElementById("delete_id").value;
     const token1 = getCookie("token1");
     const token2 = getCookie("token2");
-    
+
     const request = new Request(`${base_url}/playlist/delete-item/${id}/`, {
         method: "DELETE",
         headers: new Headers({
@@ -194,10 +281,14 @@ async function showItems() {
             }
             return response.json();
         })
-        .then((data) => {            
+        .then((data) => {
+            // if resultsJSON is not empty, clear it
+            if (resultsJSON.length > 0) {
+                resultsJSON = [];
+            }
             data.forEach((item) => {
                 // add every item to reulstsJSON
-                resultsJSON.push(item);                
+                resultsJSON.push(item);
             });
             // If the resultsJSON is not empty, show the first page
             if (resultsJSON.length > 0) {
@@ -207,11 +298,11 @@ async function showItems() {
         })
         .catch((error) => {
             console.log(error);
-        });       
+        });
 }
 
- // Function that clicks on a hidden input file element and sets its visibility back to relative
- function input_file_click(id){
+// Function that clicks on a hidden input file element and sets its visibility back to relative
+function input_file_click(id) {
     document.getElementById(id).click();
     document.getElementById(id).style = "display: relative";
 }
--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -2,21 +2,12 @@
 var current_page = 1;
 var records_per_page = 2;
 
-var objJson = [
-    { adName: "AdName 1"},
-    { adName: "AdName 2"},
-    { adName: "AdName 3"},
-    { adName: "AdName 4"},
-    { adName: "AdName 5"},
-    { adName: "AdName 6"},
-    { adName: "AdName 7"},
-    { adName: "AdName 8"},
-    { adName: "AdName 9"},
-    { adName: "AdName 10"}
-]; // Can be obtained from another source, such as your objJson variable
-
 function prevPage()
 {
+    // Unselect item for memory-game-admin.js
+    // Even if there is no item selected, the variable needs to be set to null
+    selected_item = null;
+
     if (current_page > 1) {
         current_page--;
         changePage(current_page);
@@ -25,6 +16,10 @@ function prevPage()
 
 function nextPage()
 {
+    // Unselect item for memory-game-admin.js
+    // Even if there is no item selected, the variable needs to be set to null
+    selected_item = null;
+
     if (current_page < numPages()) {
         current_page++;
         changePage(current_page);
@@ -36,36 +31,38 @@ function changePage(page)
     var btn_next = document.getElementById("btn_next");
     var btn_prev = document.getElementById("btn_prev");    
     var page_span = document.getElementById("page");
+    var total_pages_span = document.getElementById("total_pages");
 
     // Validate page
     if (page < 1) page = 1;
     if (page > numPages()) page = numPages();
 
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+    for (var i = (page - 1) * records_per_page; i < (page * records_per_page); i++) {
         // Append the items to the table
         const items = document.getElementById("listing-items");
         items.innerHTML = "";
-        resultsJSON.forEach((item) => {
+        for (var j = (page - 1) * records_per_page; j < (page * records_per_page) && j < resultsJSON.length; j++) {
+            const item = resultsJSON[j];
             const row = document.createElement("tr");
+            row.setAttribute("id", "row-" + item.id);
             const audio_file = base_url + item.audio;
             row.innerHTML = `
                 <td>${item.id}</td>
                 <td>${item.category}</td>
-                <td>${item.audio}<button value="Play" onclick="play_audio('${audio_file}')"> </td>
+                <td><button onclick="play_audio('${audio_file}')"> Play</button></td>
                 <td>${item.title}</td>
                 <td>${item.description}</td>
                 <td><img src="${base_url}${item.image}" alt="${item.title}" width="100"></td>
+                <td><span class="edit-button" onclick='editItem(${JSON.stringify(item)})'><i class="fa-solid fa-pen-to-square"></i></span></td>
+                <td><span class="delete-button" onclick="deleteItem(${item.id})"><i class="fa-solid fa-trash-can"></i></span></td>
             `;
             items.appendChild(row);
-        });
-        // listing_table.innerHTML += resultsJSON[i].id + "<br>";
-        // listing_table.innerHTML += resultsJSON[i].category + "<br>";
-        // listing_table.innerHTML += resultsJSON[i].audio + "<br>";
-        // listing_table.innerHTML += resultsJSON[i].title + "<br>";
-        // listing_table.innerHTML += resultsJSON[i].description + "<br>";
-        // listing_table.innerHTML += resultsJSON[i].image + "<br>";        
+        }
     }
+
+
     page_span.innerHTML = page;
+    total_pages_span.innerHTML = numPages();
 
     if (page == 1) {
         btn_prev.style.visibility = "hidden";
