115dbb9c59f8848d47299229c5cbdc9b9c8685a0
dimitri-edel <dimitri.edel@gmx.net>
2024-10-14 11:42:59 +0200

    Refactor changePage(page) in pagination.js

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -2,8 +2,7 @@
 var current_page = 1;
 var records_per_page = 2;
 
-function prevPage()
-{
+function prevPage() {
     // Unselect item for memory-game-admin.js
     // Even if there is no item selected, the variable needs to be set to null
     selected_item = null;
@@ -14,8 +13,7 @@ function prevPage()
     }
 }
 
-function nextPage()
-{
+function nextPage() {
     // Unselect item for memory-game-admin.js
     // Even if there is no item selected, the variable needs to be set to null
     selected_item = null;
@@ -26,10 +24,13 @@ function nextPage()
     }
 }
 
-function changePage(page)
-{
+function changePage(page) {
+    // If there are no items, return
+    if (resultsJSON.length == 0) {
+        return;
+    }
     var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");    
+    var btn_prev = document.getElementById("btn_prev");
     var page_span = document.getElementById("page");
     var total_pages_span = document.getElementById("total_pages");
 
@@ -37,29 +38,27 @@ function changePage(page)
     if (page < 1) page = 1;
     if (page > numPages()) page = numPages();
 
-    for (var i = (page - 1) * records_per_page; i < (page * records_per_page); i++) {
-        // Append the items to the table
-        const items = document.getElementById("listing-items");
-        items.innerHTML = "";
-        for (var j = (page - 1) * records_per_page; j < (page * records_per_page) && j < resultsJSON.length; j++) {
-            const item = resultsJSON[j];
-            const row = document.createElement("tr");
-            row.setAttribute("id", "row-" + item.id);
-            const audio_file = base_url + item.audio;
-            row.innerHTML = `
-                <td>${item.id}</td>
-                <td>${item.category}</td>
-                <td><button onclick="play_audio('${audio_file}')"> Play</button></td>
-                <td>${item.title}</td>
-                <td>${item.description}</td>
-                <td><img src="${base_url}${item.image}" alt="${item.title}" width="100"></td>
-                <td><span class="edit-button" onclick='editItem(${JSON.stringify(item)})'><i class="fa-solid fa-pen-to-square"></i></span></td>
-                <td><span class="delete-button" onclick="deleteItem(${item.id})"><i class="fa-solid fa-trash-can"></i></span></td>
-            `;
-            items.appendChild(row);
-        }
-    }
+    // Append the items to the table
+    const items = document.getElementById("listing-items");
+    items.innerHTML = "";
 
+    for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < resultsJSON.length; i++) {
+        const item = resultsJSON[i];
+        const row = document.createElement("tr");
+        row.setAttribute("id", "row-" + item.id);
+        const audio_file = base_url + item.audio;
+        row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.category}</td>
+            <td><button onclick="play_audio('${audio_file}')"> Play</button></td>
+            <td>${item.title}</td>
+            <td>${item.description}</td>
+            <td><img src="${base_url}${item.image}" alt="${item.title}" width="100"></td>
+            <td><span class="edit-button" onclick='editItem(${JSON.stringify(item)})'><i class="fa-solid fa-pen-to-square"></i></span></td>
+            <td><span class="delete-button" onclick="deleteItem(${item.id})"><i class="fa-solid fa-trash-can"></i></span></td>
+        `;
+        items.appendChild(row);
+    }
 
     page_span.innerHTML = page;
     total_pages_span.innerHTML = numPages();
@@ -77,11 +76,10 @@ function changePage(page)
     }
 }
 
-function numPages()
-{
+function numPages() {
     return Math.ceil(resultsJSON.length / records_per_page);
 }
 
-window.onload = function() {
+window.onload = function () {
     changePage(1);
 };
