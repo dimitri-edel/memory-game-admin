b572015f226c9a32c9290e0b4af17d42640ff86b
dimitri-edel <dimitri.edel@gmx.net>
2024-11-08 23:13:17 +0100

    Add selectItem() and unselectItem() to faces.js

--- assets/js/faces.js
+++ assets/js/faces.js
@@ -85,3 +85,55 @@ function addItem() {
         document.getElementById("image-validator").textContent = "";
     }
 }
+
+function editItem(face_id){
+    const item = apiController.faces.find(face => face.id == face_id);
+    // If there is a selected item, unselect it
+    if (selected_item !== null) {
+        unselectItem(selected_item);
+    }
+     selected_item = item;
+     selectItem(selected_item);
+}
+
+function selectItem(item) {
+    const row = document.getElementById("row-" + item.id);
+    row.innerHTML = `
+        <td>
+            <select id="category" name="category">
+                ${renderCategoryOptions()}
+            </select>
+            <span id="category-validator" class="validator-message"></span>
+        </td>
+        <td>
+            <input id="image" type="file" name="image" accept="image/*">
+            <span id="image-validator" class="validator-message"></span>
+        </td>
+        <td>
+            <span class="save-button" onclick="saveItem(${item.id})">
+                <i class="fa-solid fa-check button-icon"></i>
+            </span>
+        </td>
+        <td>
+            <span class="cancel-button" onclick="cancelEdit(${item.id})">
+                <i class="fa-solid fa-xmark button-icon"></i>
+            </span>
+        </td>`;
+}
+
+function unselectItem(item) {
+    const row = document.getElementById("row-" + item.id);
+    row.innerHTML = `
+        <td>${item.category.name}</td>
+        <td><img src="${item.image}" alt="${item.category.name}" class="face-image"></td>
+        <td>
+            <span class="edit-button" onclick="editItem(${item.id})">
+                <i class="fa-solid fa-pencil button-icon"></i>
+            </span>
+        </td>
+        <td>
+            <span class="delete-button" onclick="deleteItem(${item.id})">
+                <i class="fa-solid fa-trash button-icon"></i>
+            </span>
+        </td>`;
+}
